ba6ea6abfdea8fb3090c7fa53ada0554413115e9
Amol <amolbinwade@gmail.com>
2018-10-20 23:54:11 +0530

    updated files copy script

--- scripts/prepare_input_files.sh
+++ scripts/prepare_input_files.sh
@@ -1,17 +1,26 @@
-INPUT="/home/amol/Programs/shell_scripts/input"
-OUTPUT="/home/amol/Programs/shell_scripts/output/10142018"
 echo --------------------------------
 echo starting prepare input files
 
-echo input files in directory {$INPUT}
-ls -l $INPUT
+dt="$(date +%m)""$(date +%d)""$(date +%y)"
 
+echo Today\'s date is $dt
+
+INPUT="/home/amol/Programs/shell_scripts/input"
+OUTPUT="/home/amol/Programs/shell_scripts/output/$dt"
+
+
+
+echo input files in directory $INPUT :
+echo ------
+ls -l $INPUT
+echo ------
 echo creating output directory
 mkdir $OUTPUT
 echo created output directory $OUTPUT
-
+echo ------
 echo copying files from input directory to output directory
 for filename in "$INPUT"/*.txt; do
   cp $filename $OUTPUT
   echo copied file $filename
 done
+echo --------------------------------
